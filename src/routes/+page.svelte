5d1b2b1f4900c38f506eca77ad40d19489f03b45
chayapatr <chayapatr.arc@gmail.com>
2024-05-19 03:37:45 +0700

    feat: resize

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,9 +10,45 @@
 	let handle: any;
 	let hs: any = undefined;
 
+	let height: number, width: number;
+	let d = 0,
+		x = 400,
+		y = 200,
+		w = 192,
+		h = 192;
+
+	let isIn = (p1: any, p2: any) => {
+		return p1.x > p2.x && p1.x < p2.x + p2.width && p1.y > p2.y && p1.y < p2.y + p2.height;
+	};
+
+	let format = (p: any) => {
+		return { x: width - p.x * width, y: p.y * height };
+	};
+
+	const getDistance = (p1: any, p2: any) => {
+		return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+	};
+
 	onMount(async () => {
+		// get window size
+		height = window.innerHeight;
+		width = window.innerWidth;
+
+		// track on window size change
+		window.addEventListener('resize', () => {
+			height = window.innerHeight;
+			width = window.innerWidth;
+		});
+
+		const devices = await navigator.mediaDevices.enumerateDevices();
+		const webcam = devices.find((d) => d.label === 'FaceTime HD Camera (2C0E:82E3)');
 		navigator.mediaDevices
-			.getUserMedia({ video: true, audio: false })
+			.getUserMedia({
+				video: {
+					deviceId: webcam?.deviceId
+				},
+				audio: false
+			})
 			.then((stream) => {
 				video.srcObject = stream;
 				video.play();
@@ -46,8 +82,20 @@
 					if (video.currentTime !== lastVideoTime) {
 						const detections = handLandmarker.detectForVideo(video, video.currentTime);
 						// processResults(detections);
-						if (detections.landmarks.length > 0) hs = detections.landmarks;
-						else hs = undefined;
+						if (detections.landmarks.length > 0) {
+							hs = detections.landmarks;
+							d = getDistance(hs[0][4], hs[0][8]);
+
+							const checkPinch =
+								(isIn(format(hs?.[0]?.[4]), { x: x, y: y, width: w, height: h }) ||
+									isIn(format(hs?.[0]?.[8]), { x: x, y: y, width: w, height: h })) &&
+								d < 0.04;
+
+							if (checkPinch) {
+								w = width - hs[0][4].x * width - 0.9 * x;
+								h = hs[0][4].y * height - 0.9 * y;
+							}
+						} else hs = undefined;
 						lastVideoTime = video.currentTime;
 					}
 
@@ -61,25 +109,58 @@
 	});
 </script>
 
-<div class="flex flex-col">
-	<video playsinline bind:this={video} class="w-0 h-0">
-		Video stream not available.
-		<track kind="captions" />
-	</video>
+<div
+	class={`fixed h-[100svh] w-[100vw] top-0 left-0 flex justify-center items-end p-2 bg-neutral-950`}
+>
+	<div
+		class="bg-red-500 fixed"
+		style={`
+            width: ${w}px; height: ${h}px;
+            top: ${y}px; left: ${x}px;
+        `}
+	>
+		<div class="relative w-full h-full flex justify-center items-center">
+			<div class="flex flex-col gap-1">
+				<div>{w.toFixed(2)} x {h.toFixed(2)}</div>
+				<!-- <div>{x.toFixed(2)} x {y.toFixed(2)}</div> -->
+			</div>
+			<div
+				class="absolute right-0 bottom-0 w-12 aspect-square"
+				style={`background-color: ${
+					hs
+						? (isIn(format(hs?.[0]?.[4]), { x: x, y: y, width: w, height: h }) ||
+								isIn(format(hs?.[0]?.[8]), { x: x, y: y, width: w, height: h })) &&
+							d < 0.04
+							? 'blue'
+							: 'purple'
+						: 'purple'
+				};`}
+			></div>
+		</div>
+	</div>
 </div>
 
-<div class="relative h-[100svh] w-[100vw] flex justify-center items-center bg-neutral-950">
+<div class="relative h-[100svh] w-[100vw] flex justify-center items-center">
+	<div class="hidden fixed top-0 left-0 flex flex-col p-1 text-white gap-1">
+		<video playsinline bind:this={video} class="w-32 h-auto" style="transform: translateX(-1)">
+			Video stream not available.
+			<track kind="captions" />
+		</video>
+		<div class="flex flex-col gap-1">
+			<div>{(width || 0).toFixed(2)} x {(height || 0).toFixed(2)}</div>
+			<div>distance {d}</div>
+		</div>
+	</div>
 	{#if hs}
-		<div class=" h-full w-full" style="transform: scaleX(-1)">
+		<div class=" h-full w-full">
 			{#each hs as l}
 				{#each l as p, i}
 					<div
-						class="w-6 text-neutral-100 flex justify-center items-center aspect-square text-sm rounded-full fixed border-neutral-600 border bg-neutral-800"
-						style={`
-            left: ${p.x * 100}%;
-            top: ${p.y * 100}%;
-            transform: scaleX(-1);
-        `}
+						class={`
+                            w-6 text-neutral-100 flex justify-center items-center aspect-square text-sm rounded-full fixed border-neutral-600 border
+                            ${d < 0.04 && [4, 8].indexOf(i) !== -1 ? 'bg-red-600' : 'bg-neutral-800'}
+                        `}
+						style={` left: ${width - p.x * width}px; top: ${p.y * height}px; `}
 					>
 						{i}
 					</div>
@@ -91,11 +172,9 @@
 	{/if}
 </div>
 
-<div class={`fixed h-[100svh] w-[100vw] top-0 left-0 flex justify-center items-end p-2`}>
-	<button
-		bind:this={btn}
-		on:click={handle}
-		class={`${enabled ? 'bg-emerald-500' : 'bg-neutral-500'} text-sm text-white px-1 py-[0.15rem] rounded-md`}
-		>{enabled ? 'Running' : 'Start Camera'}</button
-	>
-</div>
+<button
+	bind:this={btn}
+	on:click={handle}
+	class={`${enabled ? 'bg-emerald-500' : 'bg-neutral-500'} text-sm text-white px-1 py-[0.15rem] rounded-md z-50 fixed bottom-0 right-0`}
+	>{enabled ? 'Running' : 'Start Camera'}</button
+>
---
+++ src/routes/wind/+page.svelte
@@ -0,0 +1,19 @@
+<div class="h-[100svh] w-[100vw] flex justify-center items-center bg-white">
+	<div
+		class="h-[720px] w-[520px] overflow-hidden p-2 border justify-center items-center bg-black flex flex-col gap-1"
+	>
+		<div class="font-mono text-red-700 text-3xl">Imposter</div>
+		<div class="text-7xl">
+			<div class="relative animate-spin">
+				<div
+					class="absolute w-6 h-3 rounded-full bg-blue-500 translate-y-7 border-2 border-black"
+				></div>
+				<div class="absolute w-4 h-2 rounded-full bg-blue-400 translate-y-[1.8rem]"></div>
+				<div class="absolute w-3 h-1 rounded-full bg-blue-300 translate-y-[1.85rem]"></div>
+				<span class="bg-gradient-to-br from-red-500 to-red-700 text-transparent bg-clip-text"
+					>ඞ</span
+				>🎒
+			</div>
+		</div>
+	</div>
+</div>
